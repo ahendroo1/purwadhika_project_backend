38d0b7ab7e8cb91888aa554197d04aa664ffe8e4
ahendroo1 <ahendroo1@gmail.com>
2018-07-05 20:55:42 +0700

    heroku commit

--- index.js
+++ index.js
@@ -1,28 +1,17 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var cors = require('cors');
-var lowdb = require('lowdb');
-var FSync = require('lowdb/adapters/FileSync');
-var adapter = new FSync('db.json');
-var db = lowdb(adapter);
 var app = express();
 
-app.use(bodyParser.json());
-// app.use(routerSpace);
 app.use(cors(0));
-// db.defaults({
-//     dataSpace:[]
-// }).write();
 
 app.get('/', (req, res)=>{
     res.send('Backend Offis');
 })
 
-
 app.post('/registrasi_user', (req, res)=>{
-    res.send()
+    res.send('send');
 })
 
-app.listen(process.env.PORT || 8080, ()=>{
-    console.log('Sever Backend Offis @port 8080');
+app.listen(3210, ()=>{
+    console.log('Sever Backend Offis @port 3210');
 })
