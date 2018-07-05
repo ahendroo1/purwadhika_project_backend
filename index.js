600d535ebceced3dd8605758a01ab31e231cc820
ahendroo1 <ahendroo1@gmail.com>
2018-07-05 22:28:33 +0700

    first commit

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var express = require('express');
+var app = express();
 var bodyParser = require('body-parser');
 var cors = require('cors');
-var app = express();
 var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://offis:1234@localhost:27017/space';
 
 app.use(bodyParser.json());
-app.use(cors(0));
+app.use(cors());
 
 app.get('/', (req, res)=>{
     res.send('Backend Offis');
@@ -23,9 +23,12 @@ app.get('/get_data_user', (req, res)=>{
 })
 
 app.post('/registrasi_user', (req, res)=>{
+        var data = { 
+            nama_lengkap: req.body.nama_lengkap, 
+            email:req.body.email, 
+        };
     MongoClient.connect(url, (err, db)=>{
         
-        var data = { nama_lengkap: req.body.nama_lengkap, email:req.body.email, no_telp: req.body.no_telp, password: req.body.password_reg };
 
         var collection = db.collection('col_user'); 
         collection.insert(data, (err, result)=>{
